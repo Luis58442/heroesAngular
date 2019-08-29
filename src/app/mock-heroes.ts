7e6a64c017bb4df000d062d6e5b84a9b0037bc00
Luis58442 <alfonso_luna1972@live.com>
2019-08-28 23:02:08 -0500

    Practica 2 de Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from '../hero';
+export const HEROES: Hero[] = [
+{ id: 1, name: 'Acuaman ' },
+{ id: 2, name: 'Flash' },
+{ id: 3, name: 'Batman' },
+{ id: 4, name: 'Superman' },
+{ id: 5, name: 'Wonder Woman ' },
+{ id: 6, name: 'Hulk' },
+{ id: 7, name: 'Black Panther' },
+{ id: 8, name: 'Capitan America' },
+{ id: 9, name: 'Iron Man ' },
+{ id: 10, name: 'ANT-MAN' }
+];
